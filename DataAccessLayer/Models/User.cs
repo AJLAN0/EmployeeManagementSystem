c9c562f2f99e7c0f5dea486345f93185eb118fc9
AJLAN <107946506+AJLAN0@users.noreply.github.com>
2024-09-03 17:52:37 +0300

    Add project files.

---
+++ DataAccessLayer/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public int RoleId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastUpdatedAt { get; set;} = DateTime.Now;
+        public Role role { get; set; } = null!;
+    }
+}
